01e2fee733c9a0a4ff2e9c9023783902b4b6ca70
William Hatcher <william@hatcher.work>
2024-10-22 14:43:42 -0500

    Commented out Google Analytics (nothing against it, just not using)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { GoogleAnalytics } from '@next/third-parties/google'
+// import { GoogleAnalytics } from '@next/third-parties/google'
 
 import localFont from "next/font/local";
 import "./globals.css";
@@ -30,7 +30,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
-      <GoogleAnalytics gaId="G-31LHNP2G97" />
+{/*      <GoogleAnalytics gaId="G-31LHNP2G97" /> */}
     </html>
   );
 }
